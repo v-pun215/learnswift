d79051c32a47bc0113d38ec8f661e34343b15ebc
v-pun215 <vihaan.pundir26@gmail.com>
2026-05-21 23:11:13 +0530

    if, switch and loops

--- hello.swift
+++ hello.swift
@@ -1,20 +1,57 @@
-enum Weekday {
-    case monday, tuesday, wednesday, thursday, friday
+let age = 16
+
+if age < 12 {
+    print("thats too bad")
+} else if age < 18 {
+    print("you can vote soon")
+} else {
+    print("go vote!")
 }
 
-var day = Weekday.monday
-print(day)
-day = .friday
+let temp = 26
+if temp > 20 && temp < 30 {
+    print("nice day")
+}
+// && is AND and || is OR, similar to c++ (or c)
+enum Weather {
+    case sun, rain, wind
+}
 
-print(day)
+let forecast = Weather.sun
 
-var score: Double = 0
-let player: String = "Peter"
-let pi: Double = 3.14
-var google: Bool = true
-var albums: Array<String> = ["Apple", "Microsoft"]
-var user: Dictionary<String, String> = ["id": "@vpun215"]
+switch forecast {
+case .sun:
+    print("bad day :(")
+case .rain:
+    print("just chillin")
+default:
+    print("cool")
+}
 
-var teams: [String] = [String]() // new array of string with no elements
-var clues = [String]() // no type annotation
+let canVote = age >= 18 ? "Yes": "No" // compact if else
+//similar to c++
 
+let platform = ["iOS", "macOS", "tvOS", "watchOS"]
+for os in platform {
+    print("Swift works on \(os)")
+}
+
+for i in 1...12 { // for i in range(1, 12)
+    print("5 x \(i) is \(5*i)")
+}
+
+var count = 10
+
+while count > 0 {
+    print("\(count)...")
+    count-=1
+}
+print("GO(lang)!")
+
+let files = ["me.jpeg", "work.txt"]
+for file in files {
+    if file.hasSuffix("jpeg") == false {
+        continue
+    }
+    print("found pic: \(file)")
+}
