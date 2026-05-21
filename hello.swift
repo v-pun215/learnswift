4b3ce824c4043ba0a4d3093b13ef6da4e5f99faa
v-pun215 <vihaan.pundir26@gmail.com>
2026-05-21 22:48:07 +0530

    string interpolation

--- hello.swift
+++ hello.swift
@@ -1,12 +1,7 @@
-let score = 10
-let high = score + 10
-let halvedScore = score/2
+//var isSaved = false
+//isSaved.toggle()
 
-var count = 10
-count += 5
-
-let num = 120
-print(num.isMultiple(of: 3))
-
-let id = Int.random(in: 1...1200)
-print("ID IS", id)
+let name = "Vihaan"
+let age = 15
+let message = "I'm \(name) and I'm \(age) years old."
+print(message)
