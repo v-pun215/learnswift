d309cc4d150ef7dd9b6671c307ba65ceb3562a21
v-pun215 <vihaan.pundir26@gmail.com>
2026-05-21 22:46:13 +0530

    ints

--- hello.swift
+++ hello.swift
@@ -1,3 +1,12 @@
-let quote = "Hello there \" Hello! \""
+let score = 10
+let high = score + 10
+let halvedScore = score/2
 
-print(quote.hasPrefix("Hello "))
+var count = 10
+count += 5
+
+let num = 120
+print(num.isMultiple(of: 3))
+
+let id = Int.random(in: 1...1200)
+print("ID IS", id)
