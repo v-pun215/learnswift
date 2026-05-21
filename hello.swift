c9ff33d28aa2b0d4851b1986e713445f3e979114
v-pun215 <vihaan.pundir26@gmail.com>
2026-05-21 22:57:59 +0530

    enums and type annotation

--- hello.swift
+++ hello.swift
@@ -1,11 +1,20 @@
-let employee = [
-    "name": "Vihaan",
-    "job": "developer"
-]
+enum Weekday {
+    case monday, tuesday, wednesday, thursday, friday
+}
 
-print(employee["job", default: "unknown"])
+var day = Weekday.monday
+print(day)
+day = .friday
 
-var numbers = Set([1, 1, 3, 4, 5, 5, 2, 1])
+print(day)
 
-print(numbers)
-print(numbers.contains(11))
+var score: Double = 0
+let player: String = "Peter"
+let pi: Double = 3.14
+var google: Bool = true
+var albums: Array<String> = ["Apple", "Microsoft"]
+var user: Dictionary<String, String> = ["id": "@vpun215"]
+
+var teams: [String] = [String]() // new array of string with no elements
+var clues = [String]() // no type annotation
+
