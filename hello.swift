3a821a7131fc600db5d7aeeeba58c03df45ce9a0
v-pun215 <vihaan.pundir26@gmail.com>
2026-05-21 22:50:41 +0530

    arrs

--- hello.swift
+++ hello.swift
@@ -1,7 +1,10 @@
-//var isSaved = false
-//isSaved.toggle()
+var colors = ["red", "green", "blue"]
+let numbers = [1, 2, 3, 4]
+var readings = [0.1, 0.5, 0.8]
 
-let name = "Vihaan"
-let age = 15
-let message = "I'm \(name) and I'm \(age) years old."
-print(message)
+print(colors)
+colors.append("purple")
+colors.remove(at:0)
+print(colors.count)
+
+print(colors.contains("october"))
